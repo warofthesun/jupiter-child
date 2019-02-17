30df14b1696d9c19c1451afaf351346a03fab0a8
Daniel Brown <fireflip@gmail.com>
2019-02-17 11:00:49 -0600

    Update thumbnail and add ACF

--- functions.php
+++ functions.php
@@ -8,4 +8,189 @@ function enqueue_parent_styles() {
 }
 
 add_image_size( 'header-image', 1200, 500, true );
+
+// 1. customize ACF path
+add_filter('acf/settings/path', 'my_acf_settings_path');
+
+function my_acf_settings_path( $path ) {
+
+    // update path
+    $path = get_stylesheet_directory() . '/acf/';
+
+    // return
+    return $path;
+
+}
+
+
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+
+function my_acf_settings_dir( $dir ) {
+
+    // update path
+    $dir = get_stylesheet_directory_uri() . '/acf/';
+
+    // return
+    return $dir;
+
+}
+
+
+// 3. Hide ACF field group menu item
+//add_filter('acf/settings/show_admin', '__return_false');
+
+
+// 4. Include ACF
+include_once( get_stylesheet_directory() . '/acf/acf.php' );
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_5c685c46a79e9',
+	'title' => 'Result Information',
+	'fields' => array(
+		array(
+			'key' => 'field_5c689d081a126',
+			'label' => 'Page Header',
+			'name' => 'page_header',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'array',
+			'preview_size' => 'blog-carousel-@2x',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+		),
+		array(
+			'key' => 'field_5c685cc7ff4b7',
+			'label' => 'Page Title',
+			'name' => 'page_title',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '30',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5c68b07c1716b',
+			'label' => 'Page Subtitle',
+			'name' => 'page_subtitle',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5c685c8dff4b6',
+			'label' => 'Result Number',
+			'name' => 'result_number',
+			'type' => 'number',
+			'instructions' => 'If this is a result page, indicate which result this is: 1–5',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '20',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'min' => '',
+			'max' => '',
+			'step' => '',
+		),
+		array(
+			'key' => 'field_5c685ddcd0105',
+			'label' => 'Result Description',
+			'name' => 'result_description',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5c68ba2142a53',
+			'label' => 'Result Email',
+			'name' => 'result_email',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page_template',
+				'operator' => '==',
+				'value' => 'page-psychographic.php',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'acf_after_title',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
+
 ?>
